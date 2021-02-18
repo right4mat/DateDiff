e1d5a3b623c0c5cc7ffeb84e3edd8021f3497a02
P2002641185 <luke.daniels@approach-solutions.com>
2021-02-18 14:56:52 +1100

    changes

--- calculator/calculator.php
+++ calculator/calculator.php
@@ -1,56 +1,51 @@
 <?php
 class DateCalculator
 {
-    private $_begda, $_endda;
+    private $_begda, $_endda, $_format;
 
-    public function __construct($begda = '', $endda = '')
+    public function __construct($format = 'd/m/Y')
     {
-
-        $this->_begda = new DateTime($begda);
-        $this->_endda = new DateTime($endda);
+        $this->_format = $format;
     }
 
     public function setBegda($begda)
     {
-        $this->_begda = new DateTime();
-        $this->_begda->setTimestamp(strtotime($begda));
+        $this->_begda = DateTime::createFromFormat($this->_format, $begda);
+        if(!$this->checkDate($this->_begda))
+            throw new Exception('Invalid beginning date');
     }
 
     public function setEndda($endda)
     {
-        $this->_endda = new DateTime();
-        $this->_endda->setTimestamp(strtotime($endda));
+        $this->_endda = DateTime::createFromFormat($this->_format, $endda);
+        if(!$this->checkDate($this->_endda))
+            throw new Exception('Invalid end date');
     }
 
     public function getbegda()
     {
-        return $this->_begda->format('d/m/Y');
+        return $this->_begda->format($this->_format);
+    
     }
 
     public function getEndda()
     {
-        return $this->_endda->format('d/m/Y');
+        return $this->_endda->format($this->_format);
     }
 
     private function checkDate($date)
     {
-        //negtive -1 check to account for old versions (we dont know which php they may use)
-
-        if ($date && ($date !== -1)) {
-            return true;
-        } else {
+        if (!$date) {
             return false;
+        } else {
+            return true;
         }
     }
 
     public function diff()
     {
-        $interval = (int)$this->_begda->diff($this->_endda)->format('%R%a') * 1; // avoid negative (test 3)
-        return $interval ? $interval-1 : 0;// if interval == 0 then the same date (dont take incomplete begda)
+        $interval = (int)$this->_begda->diff($this->_endda)->format('%a'); 
+        return $interval ? $interval-1 : 0; 
     }
 
-    public function help()
-    {
-        return "Flags: -f, -h, -l" . PHP_EOL;
-    }
 }
