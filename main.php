c9acba4234bdd73ed0fbc38452dfc79e6f5e7159
P2002641185 <luke.daniels@approach-solutions.com>
2021-02-18 22:38:58 +1100

    finished i think?

---
+++ main.php
@@ -0,0 +1,61 @@
+<?php
+require __DIR__ . '/tests/tests.php';
+
+define("BIGSPACE", PHP_EOL . PHP_EOL);
+
+$tests = new RunTests();
+
+if (!$tests->hasPassed()) {
+
+    $input = trim(readline("The calculator isnt running as expected. Do you want to proceed? (Y/N): "));
+
+    should_exist($input);
+}
+
+
+while (true) {
+    try {
+
+        $calc = new DateCalculator();
+
+        echo "Dates must be DD/MM/YYYY" . BIGSPACE;
+
+        $input = trim(readline("Beginning date: "));
+
+        echo PHP_EOL;
+
+        $calc->setBegda($input);
+
+        $input = trim(readline("End date: "));
+
+        echo PHP_EOL;
+
+        $calc->setEndda($input);
+
+        echo $calc->getBegda() . ' - ' . $calc->getEndda() . ': ' . $calc->diff() . ' days' . BIGSPACE;
+
+        $input = trim(readline("Again? (Y/N): "));
+
+        should_exist($input);
+    } catch (Exception $e) {
+
+        echo $e->getMessage() . BIGSPACE;
+
+        continue; //restart if error
+    }
+}
+
+function should_exist($input)
+{
+    if ($input === 'Y' || $input === 'y') {
+
+        echo BIGSPACE;
+
+    } else if ($input === 'N' || $input === 'n') {
+
+        exit(BIGSPACE . "Goodbye!" . BIGSPACE);
+    } else {
+
+        echo "Input not valid" . BIGSPACE;
+    }
+}
