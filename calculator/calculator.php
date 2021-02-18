d68b7bf8de5f9d078dbf6f463d2b33a307773c5d
P2002641185 <luke.daniels@approach-solutions.com>
2021-02-18 13:10:39 +1100

    changes

--- calculator/calculator.php
+++ calculator/calculator.php
@@ -1,20 +1,21 @@
 <?php
 class DateCalculator
 {
-    private $_begda, $_endda;
+    private $_begda, $_endda, $_format;
 
-    public function __construct($begda = '', $endda = '')
+    public function __construct($begda = '', $endda = '', $format = 'd/m/Y')
     {
 
         $this->_begda = strtotime($begda);
         $this->_endda = strtotime($endda);
+        $this->_format = $format;
         $this->checkDate($this->_begda);
         $this->checkDate($this->_endda);
     }
 
     public function setBegda($begda)
     {
-
+        
         $this->_begda = strtotime($begda);
         if (!$this->checkDate($this->_begda)) {
             throw new Exception('Invalid date');
@@ -23,42 +24,43 @@ class DateCalculator
 
     public function setEndda($endda)
     {
-
         $this->_endda = strtotime($endda);
         if (!$this->checkDate($this->_endda)) {
             throw new Exception('Invalid date');
         }
     }
 
-    public function getbegda()
+    public function getBegda()
     {
-        echo $this->_begda.PHP_EOL;
-        return date("d/m/Y", $this->_begda);
+        return date($this->_format, $this->_begda);
     }
 
     public function getEndda()
     {
-        echo $this->_endda.PHP_EOL;
-        return date("d/m/Y", $this->_endda);
+        return date($this->_format, $this->_endda);
     }
 
     private function checkDate($date)
     {
-        //negtive -1 check to account for old versions (we dont know which php they may use)
-
-        if ($date && ($date !== -1)) {
-            return true;
-        } else {
+        //negtive -1 check to account for old versions
+        if (!$date || ($date === -1))
             return false;
-        }
+        return true;
     }
 
     public function diff()
     {
         //minus 1 to account for begda incomplete day
-        $result = ($this->_endda - $this->_begda) / (60 * 60 * 24);
+        $result = (($this->_endda - $this->_begda) / (60 * 60 * 24)) - 1;
         //check same date to avoid -1
         $isSame = $this->_endda === $this->_begda;
-        return $isSame ? 0 : abs($result) - 1; //make sure always postive (test case 3)
+        return $isSame ? 0 : $result;
+    }
+
+    public function help()
+    {
+        return "Flags: -f, -h, -l" . PHP_EOL;
     }
 }
+// // $mycalc = new DateCalculator('01-11-1972', '01-11-1972');
+// // echo $mycalc->diff();
--- tests/tests.php
+++ tests/tests.php
@@ -2,35 +2,18 @@
 require('./calculator.php');
 
 function tests(){
-
     
 
-    if(new DateCalculator('02-06-1983', '22-06-1983')){
+    $test1 = new DateCalculator('02-06-1983', '22-06-1983');
+    
 
-    }
-
-    if(new DateCalculator('02-06-1983', '22-06-1983')){
+    $test2 = new DateCalculator('04-07-1984', '25-12-1984');
         
-    }
+    
 
-    if(new DateCalculator('02-06-1983', '22-06-1983')){
+    $test3 = new DateCalculator('03-01-1989', '03-08-1983');
         
-    }
-
-    if(new DateCalculator('02-06-1983', '22-06-1983')){
-        
-    }
-
-    if(new DateCalculator('02-06-1983', '22-06-1983')){
-        
-    }
-
-
-
-
-
-
-
+    
 
 
 }
